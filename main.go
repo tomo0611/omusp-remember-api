dc9bef5e4e9ad5d91462ca607387e527bb722906
tomo0611 <tomo0611@hotmail.com>
2024-10-16 00:44:44 +0900

    基本的なAPIができたので公開

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Member struct {
+	Grade   string `json:"grade"`
+	Name    string `json:"name"`
+	NameEn  string `json:"name_en"`
+	Bio     string `json:"bio"`
+	ImgPath string `json:"img_path"`
+}
+
+func getMembers(c echo.Context) error {
+	res, err := http.Get("https://omusp.jp/members")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	members := []Member{}
+
+	doc.Find(".user-card").Each(func(i int, s *goquery.Selection) {
+		grade := s.Find(".user-pos").Text()
+		name := s.Find(".user-name-ja").Text()
+		name_en := s.Find(".user-name-eng").Text()
+		bio := s.Find(".user-email").Text()
+		image_path := strings.Replace(s.Find("img").AttrOr("src", ""), "https://omusp.jp", "", 1)
+		member := Member{
+			Grade:   grade,
+			Name:    name,
+			NameEn:  name_en,
+			Bio:     bio,
+			ImgPath: image_path,
+		}
+		members = append(members, member)
+	})
+
+	return c.JSON(http.StatusOK, struct {
+		Members []Member `json:"members"`
+	}{Members: members})
+}
+
+func main() {
+	e := echo.New()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogError:    true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+				)
+			} else {
+				logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("err", v.Error.Error()),
+				)
+			}
+			return nil
+		},
+	}))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello, Azure Container Apps!")
+	})
+
+	e.GET("/api/members", getMembers)
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+	e.Logger.Fatal(e.Start(":8000"))
+}
